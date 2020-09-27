b5d9c807d411539126d0d8e6a650229f99688a41
Ishwor Khadka <ishworkh@gmail.com>
2020-09-27 03:42:47 +0300

    Add main module image_artifact

---
+++ src/image_artifact.js
@@ -0,0 +1,45 @@
+const docker = require('./docker');
+const artifact = require('./github_artifact');
+
+const INVALID_CHARS = /[\s><:"|*?/\\]/g;
+
+const resolvePackageName = (imageName) => imageName.replace(INVALID_CHARS, '_');
+
+const resolveArtifactName = (imageName) => `action_image_artifact_${sanitiseName(imageName)}`;
+
+/**
+ * @param {string} image 
+ * 
+ * @returns {string} // Uploaded artifact name
+ * 
+ * Upload a image package as github artifact which is later possible to point to with given image name. 
+ * Eg. image "foo:latest" is packaged to a file `foo_latest` and then upload as an artifact with a name 
+ *      `image_artifact_foo_latest`. In short we will have an artifact like,
+ *             image_artifact_foo_latest[foo_latest]
+ */
+exports.upload = async function(image) {
+    const packagePath = await docker.packageImage(image, path.join(os.tmpdir(), resolvePackageName(image)));
+    
+    const artifactName = resolveArtifactName(image);
+    await artifact.upload(artifactName, packagePath);
+
+    return artifactName;
+}
+
+/**
+ * @param {string} image  
+ * 
+ * @returns {string} // Artifact local downloaded path 
+ * 
+ * For a github artifact to be linked with the given image name, it has to be named in predictable way.
+ * Eg. image `foo:latest` packaged as `foo_latest` uploaded as an artifact named `image_artifact_foo_latest`
+ *      can be downloaded and loaded with ${downloadDir}/${packageName} i.e /tmp/foo_latest
+ */
+exports.download = async function(image) {
+    const downloadDir = await artifact.download(resolveArtifactName(image), os.tmpdir());
+    
+    const imagePackageName = resolvePackageName(image);    
+    await docker.loadImage(downloadDir, imagePackageName);  
+
+    return path.join(downloadDir, imagePackageName);
+}
